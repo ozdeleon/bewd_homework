a9c0bc4a1b6bbdaa814e1a64fa17561b8a10f0dc
ozdeleon <ozdeleon@icloud.com>
2015-09-13 01:01:40 -0700

    Who am I edited.

--- who_am_i_all_lowercase.rb
+++ who_am_i_all_lowercase.rb
@@ -1,17 +1,13 @@
 puts "What is your name"
 name = gets.chomp
 
-
 puts "Where did you grow up?"
 location = gets.chomp
-
 
 puts "What is your current occupation?"
 occupation = gets.chomp
 
-
 puts "What is your hidden talent?"
 talent = gets.chomp
-
 
 puts  "My name is #{name}. I grew up in #{location}. I currently work as a #{occupation} and I #{talent}.".downcase
