adc04722a8f4f27acd9217be7b227ee40dbc7752
ozdeleon <ozdeleon@icloud.com>
2015-09-19 14:05:18 -0700

    Added chains exercise

---
+++ class_3_4_homework/chains.rb
@@ -0,0 +1,6 @@
+puts "Please enter a sentence"
+sentence = gets.chomp
+puts "#{sentence}"
+puts "#{sentence.reverse.upcase}"
+puts "#{sentence.length * 2}"
+puts "#{sentence.split.first.capitalize}"
